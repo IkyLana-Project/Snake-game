0f87ab4737ccad2a3045c6c3f6f0b9c060be8035
M.Rizky Maulana <127418030+IkyLana-Project@users.noreply.github.com>
2025-03-04 08:51:33 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-let canvas = document.getElementById("snake"); //criar elemento que irá rodar o jogo
+let canvas = document.getElementById("snake");
 let context = canvas.getContext("2d");
 let box = 32;
 let snake = [];
